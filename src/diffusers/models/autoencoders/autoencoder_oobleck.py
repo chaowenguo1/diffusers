467f8fc3b770cf9f13f2261fe29b66bf9b17ef5f
chaowenguo1 <chaowen.guo1@gmail.com>
2024-12-04 22:52:47 -0800

    Update autoencoder_oobleck.py

--- src/diffusers/models/autoencoders/autoencoder_oobleck.py
+++ src/diffusers/models/autoencoders/autoencoder_oobleck.py
@@ -136,6 +136,7 @@ class OobleckDecoderBlock(nn.Module):
 
     def forward(self, hidden_state):
         hidden_state = self.snake1(hidden_state)
+        print(hidden_state)
         hidden_state = self.conv_t1(hidden_state)
         hidden_state = self.res_unit1(hidden_state)
         hidden_state = self.res_unit2(hidden_state)
